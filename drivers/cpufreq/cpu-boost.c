53fb6ed3e7085a514fb82ab19148e3eafd09d41a
joshuous <joshuous@gmail.com>
2018-02-09 22:52:10 +0800

    cpu-boost: Implement Dynamic SchedTune Boost v3
    Dynamic SchedTune Boost occurs on-the-fly during interactions, such
    as touch input. This idea was conceived because of an existing battery
    drain issue whereby setting a permanent schedtune boost would cause
    boosting to occur even when the phone was idling. For example, if a user
    sets the /dev/stune/top-app/schedtune.boost to 10, the device may not
    idle at the lowest frequency step, which can lead to higher idle and
    active drain rates. Thus it should consume less power to boost only
    during interactions.
    
    v1 worked by changing the value returned by schedtune_cpu_boost(), but
    did not change the true boost value of the top-app cgroup. In comparison,
    v2 modifies the value in /dev/stune/top-app/schedtune.boost directly.
    v3 reworks the entire Dynamic SchedTune Boost code and improves detection
    of top-app or any other SchedTune group.
    
    The tunable can be found in /sys/module/cpu_boost/parameters/dynamic_stune_boost.
    
    (cherry picked from commit 1f5e3e2a91afea554febe74f545f576e4eb36d4b)
    (cherry picked from commit 6ecce27be44ec40afb19025447f578f57e9fa258)

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -46,6 +46,11 @@ static bool input_boost_enabled;
 static unsigned int input_boost_ms = 200;
 module_param(input_boost_ms, uint, 0644);
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+static int dynamic_stune_boost;
+module_param(dynamic_stune_boost, uint, 0644);
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 static bool sched_boost_on_input;
 module_param(sched_boost_on_input, bool, 0644);
 
@@ -221,6 +226,11 @@ static void do_input_boost_rem(struct work_struct *work)
 		i_sync_info->input_boost_min = 0;
 	}
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	/* Reset dynamic stune boost value to the default value */
+	reset_stune_boost("top-app");
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 	/* Update policies for all online CPUs */
 	update_policy_online();
 
@@ -277,6 +287,11 @@ static int do_input_boost(void *data)
 			else
 				sched_boost_active = true;
 		}
+
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	/* Set dynamic stune boost value */
+	do_stune_boost("top-app", dynamic_stune_boost);
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 bail_incorrect_governor:
 		put_online_cpus();
@@ -355,6 +370,11 @@ err2:
 
 static void cpuboost_input_disconnect(struct input_handle *handle)
 {
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	/* Reset dynamic stune boost value to the default value */
+	reset_stune_boost("top-app");
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 	input_close_device(handle);
 	input_unregister_handle(handle);
 	kfree(handle);
