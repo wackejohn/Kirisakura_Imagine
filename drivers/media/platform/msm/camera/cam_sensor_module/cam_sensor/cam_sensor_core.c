e268b7e9c8919d76c3778050668afdd793a86673
Karthik Anantha Ram <kartanan@codeaurora.org>
2018-05-24 15:11:39 -0700

    msm: camera: sensor: Validate power settings
    As part of shutdown when we free the power settings we should
    assign the pointer to NULL. And in power down we validate the
    settings.
    
    Change-Id: I7abe11548e211dfd89387069191234488dcfd0ce
    Signed-off-by: Karthik Anantha Ram <kartanan@codeaurora.org>
    (cherry picked from commit b5e135c0fb2ce27a500ede21e049c9346ea96dd1)

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_sensor/cam_sensor_core.c
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_sensor/cam_sensor_core.c
@@ -624,6 +624,8 @@ void cam_sensor_shutdown(struct cam_sensor_ctrl_t *s_ctrl)
 
 	kfree(power_info->power_setting);
 	kfree(power_info->power_down_setting);
+	power_info->power_setting = NULL;
+	power_info->power_down_setting = NULL;
 
 	s_ctrl->streamon_count = 0;
 	s_ctrl->streamoff_count = 0;
@@ -699,26 +701,6 @@ int32_t cam_sensor_driver_cmd(struct cam_sensor_ctrl_t *s_ctrl,
 			break;
 		}
 		CAM_INFO(CAM_SENSOR, "CAM_SENSOR_PROBE_CMD"); //HTC_ADD
-		/* Allocate memory for power up setting */
-		pu = kcalloc(MAX_POWER_CONFIG,
-			     sizeof(struct cam_sensor_power_setting),
-			     GFP_KERNEL);
-		if (!pu) {
-			rc = -ENOMEM;
-			goto release_mutex;
-		}
-
-		pd = kcalloc(MAX_POWER_CONFIG,
-			     sizeof(struct cam_sensor_power_setting),
-			     GFP_KERNEL);
-		if (!pd) {
-			kfree(pu);
-			rc = -ENOMEM;
-			goto release_mutex;
-		}
-
-		power_info->power_setting = pu;
-		power_info->power_down_setting = pd;
 
 		if (cmd->handle_type ==
 			CAM_HANDLE_MEM_HANDLE) {
@@ -734,6 +716,9 @@ int32_t cam_sensor_driver_cmd(struct cam_sensor_ctrl_t *s_ctrl,
 				 cmd->handle_type);
 			return -EINVAL;
 		}
+
+		pu = power_info->power_setting;
+		pd = power_info->power_down_setting;
 
 		/* Parse and fill vreg params for powerup settings */
 		rc = msm_camera_fill_vreg_params(
