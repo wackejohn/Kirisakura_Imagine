0b993a666825b25cc20b3baa1307c7b04b731e33
freak07 <michalechner92@googlemail.com>
2018-06-27 12:14:13 +0200

    module.c: add texfat module to be able to be loaded on boot again

--- kernel/module.c
+++ kernel/module.c
@@ -1277,9 +1277,6 @@ static int check_version(Elf_Shdr *sechdrs,
 {
 	unsigned int i, num_versions;
 	struct modversion_info *versions;
-
-	if(!strncmp("opchain", mod->name, 7))
-		return 1;
 	
 	if(!strncmp("wlan", mod->name, 4))
 		return 1;
@@ -1315,6 +1312,9 @@ static int check_version(Elf_Shdr *sechdrs,
 		return 1;
 
 	if(!strncmp("pinctrl_wcd", mod->name, 11))
+		return 1;
+		
+	if(!strncmp("texfat", mod->name, 6))
 		return 1;
 	
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
