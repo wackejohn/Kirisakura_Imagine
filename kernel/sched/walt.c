6d517f09c29587a32050324be8363bed83c9df13
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2018-07-20 15:23:35 +0530

    sched: Fix lockdep warning for rq locks
    WALT tries to acquire all possible cpu's runqueue locks to update
    cluster stats or capacities. When lockdep enabled, it reports below
    warning.
    
    Fix it by using raw_spinlock_nested API to inform lockdep it is
    intentional.
    
    Report:
    
    [1.365691] May be due to missing lock nesting notation\x0a
    [1.372528] 1 lock held by swapper/0/1:
    [1.376399] #0:  (&rq->lock){-.-...}, at: [<ffffff98605282b0>]
        	acquire_rq_locks_irqsave.constprop.22+0x64/0x9c
    [1.386649] \x0astack backtrace:
    [1.391054] CPU: 0 PID: 1 Comm: swapper/0 Not tainted 4.9.106+ #10
    [1.397282] Hardware name: Qualcomm Technologies, Inc. SDM845 v2.1 MTP (DT)
    [1.404295] Call trace:
    [1.406769] [<ffffff986048c2bc>] dump_backtrace+0x0/0x2c0
    [1.412217] [<ffffff986048c59c>] show_stack+0x20/0x28
    [1.417315] [<ffffff98608ba190>] dump_stack+0xa8/0xe0
    [1.422412] [<ffffff9860542118>] __lock_acquire+0x18fc/0x1d74
    [1.428204] [<ffffff9860542964>] lock_acquire+0xe0/0x288
    [1.433569] [<ffffff986172263c>] _raw_spin_lock+0x44/0x58
    [1.439009] [<ffffff98605282b0>] acquire_rq_locks_irqsave.constprop.22+0x64/0x9c
    [1.446458] [<ffffff9860528324>] update_all_clusters_stats+0x3c/0x1f8
    [1.452953] [<ffffff986052c494>] update_cluster_topology+0x2a0/0x300
    [1.459363] [<ffffff9861fc456c>] sched_init_smp+0xc4/0x11c
    [1.464896] [<ffffff9861fb0e48>] kernel_init_freeable+0x150/0x2bc
    [1.471044] [<ffffff98617198d4>] kernel_init+0x18/0x100
    [1.476308] [<ffffff9860483f00>] ret_from_fork+0x10/0x50
    
    Change-Id: Ibc5726e0f50f0135fd9b46c8065ee0f52dd5d5e7
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    (cherry picked from commit 071be09fac6091a1c1793d4bc0ef92de80dd4619)

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -53,11 +53,16 @@ static struct irq_work walt_migration_irq_work;
 static void acquire_rq_locks_irqsave(const cpumask_t *cpus,
 				     unsigned long *flags)
 {
-	int cpu;
+	int cpu, level = 0;
 
 	local_irq_save(*flags);
-	for_each_cpu(cpu, cpus)
-		raw_spin_lock(&cpu_rq(cpu)->lock);
+	for_each_cpu(cpu, cpus) {
+		if (level == 0)
+			raw_spin_lock(&cpu_rq(cpu)->lock);
+		else
+			raw_spin_lock_nested(&cpu_rq(cpu)->lock, level);
+		level++;
+	}
 }
 
 static void release_rq_locks_irqrestore(const cpumask_t *cpus,
