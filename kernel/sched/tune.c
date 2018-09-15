85c7e728085df30011777fcf7806f725e484d0e2
freak07 <michalechner92@googlemail.com>
2018-09-15 13:24:29 +0200

    tune.c fix forbidden ISO 90 warning

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -1137,10 +1137,6 @@ out:
 static void
 schedtune_boostgroup_release(struct schedtune *st)
 {
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	/* Free dynamic boost slots */
-	boost_slots_release(st);
-#endif // CONFIG_DYNAMIC_STUNE_BOOST
 	struct boost_groups *bg;
 	int cpu;
 
@@ -1153,6 +1149,11 @@ schedtune_boostgroup_release(struct schedtune *st)
 
 	/* Keep track of allocated boost groups */
 	allocated_group[st->idx] = NULL;
+	
+	#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	/* Free dynamic boost slots */
+	boost_slots_release(st);
+	#endif // CONFIG_DYNAMIC_STUNE_BOOST
 }
 
 static void
