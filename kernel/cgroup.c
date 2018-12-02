f638143dc3819eb27cf11bf1b902be78d964c1c5
freak07 <michalechner92@googlemail.com>
2018-12-02 09:25:52 +0100

    cgroup: change boost for pixel launcher app to HTCs stock launcher app

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2961,7 +2961,7 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 		ret = cgroup_attach_task(cgrp, tsk, threadgroup);
 
 	/* Boost CPU to the max for 500 ms when launcher becomes a top app */
-	if (!memcmp(tsk->comm, "s.nexuslauncher", sizeof("s.nexuslauncher")) &&
+	if (!memcmp(tsk->comm, "htc.launcher", sizeof("htc.launcher")) &&
 		!memcmp(cgrp->kn->name, "top-app", sizeof("top-app")) && !ret) {
 		cpu_input_boost_kick_max(1000);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1000);
