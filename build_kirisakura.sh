68d9c01b6e0695947d23880577a3d06aa59805d0
freak07 <michalechner92@googlemail.com>
2020-11-28 17:22:40 +0100

    buildscript: adjust for new CPU

--- build_kirisakura.sh
+++ build_kirisakura.sh
@@ -21,4 +21,4 @@ make ARCH=arm64 CROSS_COMPILE=~/Android_Build/GCC_Google_Arm64/aarch64-linux-and
 echo
 echo "Build The Good Stuff"
 echo
-make ARCH=arm64 CROSS_COMPILE=~/Android_Build/GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android- CROSS_COMPILE_ARM32=~/Android_Build/GCC_Google_Arm32/arm-linux-androideabi-4.9/bin/arm-linux-androideabi- O=out -j4
+make ARCH=arm64 CROSS_COMPILE=~/Android_Build/GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android- CROSS_COMPILE_ARM32=~/Android_Build/GCC_Google_Arm32/arm-linux-androideabi-4.9/bin/arm-linux-androideabi- O=out -j24
